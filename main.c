0135b934f04dbbf39d38202dd357272871b8c140
jiwonryu <rjwbp1@naver.com>
2018-11-22 16:35:34 +0900

    파일 복사 프로그램

--- main.c
+++ main.c
@@ -4,17 +4,30 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main (void){
-	FILE *fp;
+	FILE *fp, *fp2;
 	char input;
+	char original[100];
+	char copy[100];
+	int i=0;
 	
-	fp= fopen("sample.txt","r");
+	printf("original file: ");
+	scanf("%s", original);
+	printf("copy file: ");
+	scanf("%s", copy);
 	
-	while ( (input = fgetc(fp)) != EOF )
+	fp= fopen(original, "r");
+	fp2= fopen(copy,"w");
+	
+	while( (input = fgetc(fp)) != EOF)
 	{
-		putchar(input);
+		fputc(input,fp2);
+		i++;
 	}
 	
+	printf("Copy succeed! (%i Bytes copied)\n", i);
+	
 	fclose(fp);
+	fclose(fp2);
 	
 	return 0;
 }
